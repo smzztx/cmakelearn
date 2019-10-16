c2345729320896140e1e2edf60d672b0edcbadbb
tangxiang <tangxiang007@gmail.com>
2019-10-16 08:33:37 -0700

    update main.cc; add demo6

--- demo5/main.cc
+++ demo5/main.cc
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "math/MathFunctions.h"
+#include <config.h>
+
+#ifdef USE_MYMATH
+  #include <MathFunctions.h>
+#else
+  #include <math.h>
+#endif
+
 
 int main(int argc, char *argv[])
 {
@@ -8,10 +15,18 @@ int main(int argc, char *argv[])
         printf("Usage: %s base exponent \n", argv[0]);
         return 1;
     }
+
     double base = atof(argv[1]);
     int exponent = atoi(argv[2]);
+
+#ifdef USE_MYMATH
+    printf("Now we use our own Math library. \n");
     double result = power(base, exponent);
-    printf("%g ^ %d is %g\n", base, exponent, result);
+#else
+    printf("Now we use the standard library. \n");
+    double result = pow(base, exponent);
+#endif
     
+    printf("%g ^ %d is %g\n", base, exponent, result);
     return 0;
 }
--- demo6/main.cc
+++ demo6/main.cc
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include <config.h>
 
-#ifdef USE_MYMATH
-  #include <MathFunctions.h>
-#else
+#ifdef HAVE_POW
   #include <math.h>
+#else
+  #include <MathFunctions.h>
 #endif
-
 
 int main(int argc, char *argv[])
 {
@@ -15,16 +14,15 @@ int main(int argc, char *argv[])
         printf("Usage: %s base exponent \n", argv[0]);
         return 1;
     }
-
     double base = atof(argv[1]);
     int exponent = atoi(argv[2]);
-
-#ifdef USE_MYMATH
-    printf("Now we use our own Math library. \n");
-    double result = power(base, exponent);
-#else
+    
+#ifdef HAVE_POW
     printf("Now we use the standard library. \n");
     double result = pow(base, exponent);
+#else
+    printf("Now we use our own Math library. \n");
+    double result = power(base, exponent);
 #endif
     
     printf("%g ^ %d is %g\n", base, exponent, result);
